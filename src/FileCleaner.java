068b0e02296e17983a843701cb6331d321ed797e
Kyle Buscaglia <kyle@rosieapp.com>
2018-10-06 10:45:14 -0400

    Adjusting for new file format with multiple sale dates in one file

--- src/FileCleaner.java
+++ src/FileCleaner.java
@@ -3,14 +3,21 @@ import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class FileCleaner {
 
-  private static final String IN_FILE_NAME = "C:\\Repos\\TestFiles\\in_201810030301_cswg_Stauffers54665_sw_prices.txt";
-  private static final String OUT_FILE_NAME = "C:\\Repos\\TestFiles\\Fixed_201810030301_cswg_Stauffers54665_sw_prices.txt";
+  private static final String IN_FILE_NAME = "C:\\Repos\\TestFiles\\201810051502_cswg_Stauffers53340_sw_prices.txt";
+  private static final String OUT_FILE_NAME = "C:\\Repos\\TestFiles\\Fixed_201810051502_cswg_Stauffers53340_sw_prices.txt.txt";
   private String[] outColumns = new String[25];
   private String[] previousColumns;
   private String[] currentColumns;
+  private String[] basePriceRow;
+  private ArrayList<String[]> fsRows = new ArrayList<>();
+  private ArrayList<String[]> saleRows = new ArrayList<>();
+  private ArrayList<String[]> tprRows = new ArrayList<>();
+  private ArrayList<String[]> outRows = new ArrayList<>();
 
   public void cleanFile(){
     try (BufferedReader br = new BufferedReader(new FileReader(IN_FILE_NAME))) {
@@ -24,17 +31,21 @@ public class FileCleaner {
           currentColumns = currentLine.split("\\|");
           //handle base case/ first line
           if (previousColumns == null) {
+            //System.out.println("PREV COLUMNS NULL");
             previousColumns = currentColumns;
             continue;
           }
 
           if (currentColumns[0].equals(previousColumns[0])) {
-            //set the outColumns to current so that outColumns that are not over written in
+            //set the outColumns to current so that outColumn entries that are not over written in
             //copyNeededColumns are still filled in
+            //System.out.println("Matching rows");
+            //System.out.println(Arrays.toString(currentColumns));
             outColumns = currentColumns;
             combineRows(currentColumns, previousColumns, br, bw);
           } else {
             //the current row did not match the previous write out
+            //System.out.println("Writing unique row");
             bw.write(String.join("|", previousColumns));
             bw.newLine();
             previousColumns = currentColumns;
@@ -42,14 +53,15 @@ public class FileCleaner {
 
         }
 
-        if (outColumns[0].equals(previousColumns[0])) {
-          //if the last rows were all the same product
-          bw.write(String.join("|", outColumns));
-        } else {
+//        if (outColumns[0].equals(previousColumns[0])) {
+//          //if the last rows were all the same product
+//          System.out.println("writing last line");
+//          bw.write(String.join("|", outColumns));
+//        } else {
           //if the last row was a unique product
-          bw.write(String.join("|", previousColumns));
-        }
-        bw.newLine();
+          //bw.write(String.join("|", previousColumns));
+        //}
+        //bw.newLine();
       }
 
     } catch (IOException e) {
@@ -69,65 +81,194 @@ public class FileCleaner {
                            BufferedReader br,
                            BufferedWriter bw) {
 
-    copyNeededColumns(currentColumns);
-    copyNeededColumns(previousColumns);
+
+    //if (isBasePrice(previousColumns)) {
+    //  basePriceRow = previousColumns;
+    //} else if (isBasePrice(currentColumns)) {
+    //  basePriceRow = currentColumns;
+    //} else {
+      populateRowLists(previousColumns);
+      populateRowLists(currentColumns);
+      //System.out.println("finding base");
+      //System.out.println(Arrays.toString(currentColumns));
+      findBasePrice(br , previousColumns[0]);
+   // }
+
+    if (basePriceRow == null) {
+      System.out.println("Should not happen base price was null");
+      System.out.println(Arrays.toString(currentColumns));
+      //writeOutRows();
+    } else {
+
+      if (fsRows.size() >= saleRows.size() && fsRows.size() >= tprRows.size() ) {
+        handleFsRows();
+      } else if (saleRows.size() >= fsRows.size() && saleRows.size() >= tprRows.size()) {
+        handleSaleRows();
+      } else if (tprRows.size() > saleRows.size() &&  tprRows.size() >= fsRows.size()) {
+        handleTprRows();
+      } else {
+        System.out.println("Should not happen..");
+      }
+
+      for(String[] outRow : outRows) {
+        try {
+          //System.out.println("Writing out rows");
+          bw.write(String.join("|", outRow));
+          bw.newLine();
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
+      resetRows();
+    }
+
+  }
+
+  public void resetRows() {
+    //System.out.println("reseting base price row");
+    //System.out.println(Arrays.toString(basePriceRow));
+    basePriceRow = null;
+    outRows.clear();
+    fsRows.clear();
+    saleRows.clear();
+    tprRows.clear();
+  }
+
+  private void findBasePrice (BufferedReader br, String productId) {
 
     try {
       String currentLine;
 
-      if ((currentLine = br.readLine()) != null) {
-        String[] nextColumns = currentLine.split("\\|");
-        if ( nextColumns[0].equals(currentColumns[0])) {
-          //continue to combine rows and read the next line until
-          //we get to a new product
-          combineRows(previousColumns, nextColumns, br, bw);
+      while ((currentLine = br.readLine()) != null) {
+        String[] columns = currentLine.split("\\|");
+
+        if (columns[0].equals(productId)) {
+         populateRowLists(columns);
         } else {
-          //current line is now for a new product, write out the result of combining the rows
-          bw.write(String.join("|", outColumns));
-          bw.newLine();
-          //adjust the previous columns to keep them in sync with outer loop
-          this.currentColumns = previousColumns;
-          this.previousColumns = nextColumns;
+          this.previousColumns = columns;
+          break;
         }
+
       }
-    } catch (IOException ex) {
+
+    } catch( IOException ex) {
       ex.printStackTrace();
     }
+
   }
 
-  /**
-   * Use the quantity column to determine the type of row that is being processed
-   *
-   * @param columns
-   */
-  private void copyNeededColumns(String[] columns) {
-    //Base Price Row
+  private void populateRowLists(String[] columns) {
+
     if(!columns[5].equals("0") && !columns[5].equals("")) {
-      outColumns[5] = columns[5];
-      outColumns[6] = columns[6];
-      outColumns[7] = columns[7];
+      basePriceRow = columns;
     }
-    //FS Pricing Row
-    if (!columns[24].equals("0") && !columns[24].equals("")) {
-      outColumns[24] = columns[24];
-      outColumns[23] = columns[23];
-      outColumns[22] = columns[22];
+    else if (!columns[24].equals("0") && !columns[24].equals("")) {
+      fsRows.add(columns);
     }
-    //Sale Price Row
-    if (!columns[12].equals("0") && !columns[12].equals("")) {
-      outColumns[12] = columns[12];
-      outColumns[13] = columns[13];
-      outColumns[14] = columns[14];
-      outColumns[15] = columns[15];
+    else if (!columns[12].equals("0") && !columns[12].equals("")) {
+      saleRows.add(columns);
     }
-    //TPR Row
-    if (!columns[8].equals("0") && !columns[8].equals("")) {
-      outColumns[8] = columns[8];
-      outColumns[9] = columns[9];
-      outColumns[10] = columns[10];
-      outColumns[11] = columns[11];
+    else if (!columns[8].equals("0") && !columns[8].equals("")) {
+      tprRows.add(columns);
     }
 
   }
+
+  private void handleFsRows(){
+    for (String[] fsRow : fsRows) {
+      String[] outRowEntry = combineFsWithBaseRow(fsRow, null);
+
+      outRows.add(outRowEntry);
+    }
+    for(int i = 0; i < saleRows.size(); i++) {
+      combineSaleWithBaseRow(saleRows.get(i), outRows.get(i));
+    }
+    for(int i = 0; i < tprRows.size(); i++) {
+      combineTprWithBaseRow(tprRows.get(i), outRows.get(i));
+    }
+  }
+
+  private void handleSaleRows() {
+
+    for (String[] saleRow : saleRows) {
+      String[] outRowEntry = combineSaleWithBaseRow(saleRow, null);
+
+      outRows.add(outRowEntry);
+    }
+    for(int i = 0; i < fsRows.size(); i++) {
+      combineFsWithBaseRow(fsRows.get(i), outRows.get(i));
+    }
+    for(int i = 0; i < tprRows.size(); i++) {
+      combineTprWithBaseRow(tprRows.get(i), outRows.get(i));
+    }
+
+
+  }
+
+  private void handleTprRows() {
+    for (String[] tprRow : tprRows) {
+      String[] outRowEntry = combineTprWithBaseRow(tprRow, null);
+
+      outRows.add(outRowEntry);
+    }
+    for(int i = 0; i < fsRows.size(); i++) {
+      combineFsWithBaseRow(fsRows.get(i), outRows.get(i));
+    }
+    for(int i = 0; i < saleRows.size(); i++) {
+      combineSaleWithBaseRow(saleRows.get(i), outRows.get(i));
+    }
+  }
+
+  private String[] combineFsWithBaseRow(String[] row, String[] outRowEntry) {
+
+    if(outRowEntry == null){
+      outRowEntry = basePriceRow.clone();
+    }
+    outRowEntry[24] = row[24];
+    outRowEntry[23] = row[23];
+    outRowEntry[22] = row[22];
+
+    return outRowEntry;
+
+  }
+
+  private String[] combineSaleWithBaseRow(String[] row, String[] outRowEntry) {
+
+    if(outRowEntry == null){
+      outRowEntry = basePriceRow.clone();
+    }
+
+    outRowEntry[12] = row[12];
+    outRowEntry[13] = row[13];
+    outRowEntry[14] = row[14];
+    outRowEntry[15] = row[15];
+
+    return outRowEntry;
+
+  }
+
+  private String[] combineTprWithBaseRow(String[] row, String[] outRowEntry) {
+
+    if(outRowEntry == null){
+      outRowEntry = basePriceRow.clone();
+    }
+
+    outRowEntry[8] = row[8];
+    outRowEntry[9] = row[9];
+    outRowEntry[10] = row[10];
+    outRowEntry[11] = row[11];
+
+    return outRowEntry;
+
+  }
+
+  private boolean isBasePrice (String[] columns) {
+
+    if(!columns[5].equals("0") && !columns[5].equals("")) {
+      return true;
+    }
+    return false;
+  }
+
 
 }
